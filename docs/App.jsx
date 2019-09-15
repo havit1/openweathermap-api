f7241c29d7d776d721fd5620e0300537866e9cca
Oleksandr <sashaka2013@gmail.com>
2019-09-15 18:13:45 +0300

    testing

---
+++ docs/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import PageContent from "./components/PageContent/PageContent";
+
+function App() {
+  return (
+    <div>
+      <PageContent />
+    </div>
+  );
+}
+
+export default App;
