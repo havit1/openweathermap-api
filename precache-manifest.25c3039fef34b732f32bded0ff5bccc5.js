9cefbba17c3cceeeb2a803e14e6a579112baf8f6
Oleksandr <sashaka2013@gmail.com>
2019-11-09 18:38:11 +0200

    Updates

---
+++ precache-manifest.25c3039fef34b732f32bded0ff5bccc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0037100001089b42cb8750618eabbde",
+    "url": "/openweathermap-api/index.html"
+  },
+  {
+    "revision": "ff0b45a966775f6237b5",
+    "url": "/openweathermap-api/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "8c7fc6be48be7c099808",
+    "url": "/openweathermap-api/static/css/main.95abad8d.chunk.css"
+  },
+  {
+    "revision": "ff0b45a966775f6237b5",
+    "url": "/openweathermap-api/static/js/2.ab9e3110.chunk.js"
+  },
+  {
+    "revision": "8c7fc6be48be7c099808",
+    "url": "/openweathermap-api/static/js/main.826b5ddf.chunk.js"
+  },
+  {
+    "revision": "1e6491e31745f8fda1f5",
+    "url": "/openweathermap-api/static/js/runtime~main.c3995d25.js"
+  }
+]);
