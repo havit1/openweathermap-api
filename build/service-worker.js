0b9a81baffd5d7dbcccf22f09d18abba7629a091
Oleksandr <sashaka2013@gmail.com>
2019-11-09 18:48:56 +0200

    Revert "Update"
    This reverts commit bd8abf06194e9e334f134a88688b9102d6066a26.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/openweathermap-api/precache-manifest.25c3039fef34b732f32bded0ff5bccc5.js"
+  "/precache-manifest.27b4ab8bcd0d6b48e9d4ea154f3aabf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/openweathermap-api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
