5a5cb802c194f2301e7d0f466eb62db984045494
Oleksandr <sashaka2013@gmail.com>
2019-12-18 12:17:28 +0200

    Updates

---
+++ precache-manifest.468bc0cfcb9c40f4cd42ac798cabaf04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b848dde8f39d60b99c8b1aaed0842777",
+    "url": "/openweathermap-api/index.html"
+  },
+  {
+    "revision": "0f1c1d30792456218121",
+    "url": "/openweathermap-api/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "ab7d191f6764f352baa3",
+    "url": "/openweathermap-api/static/css/main.73ecd0a0.chunk.css"
+  },
+  {
+    "revision": "0f1c1d30792456218121",
+    "url": "/openweathermap-api/static/js/2.b7d05869.chunk.js"
+  },
+  {
+    "revision": "ab7d191f6764f352baa3",
+    "url": "/openweathermap-api/static/js/main.cb4370b2.chunk.js"
+  },
+  {
+    "revision": "1e6491e31745f8fda1f5",
+    "url": "/openweathermap-api/static/js/runtime~main.c3995d25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/openweathermap-api/precache-manifest.718aa9c83d4227dbbf9e0302e15656bf.js"
+  "/openweathermap-api/precache-manifest.468bc0cfcb9c40f4cd42ac798cabaf04.js"
 );
 
 self.addEventListener('message', (event) => {
